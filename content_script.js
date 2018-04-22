2a3a75aeb72269dc045fe0edaaaf372c449ca5c2
Tyler Rand <tyler@tyler-rand.com>
2018-04-21 22:46:05 -0400

    Hide modal and enable scrolling

---
+++ content_script.js
@@ -0,0 +1,6 @@
+// remove modal hiding profile info
+modal = document.getElementById("advocate-modal");
+modal.parentNode.removeChild(modal);
+
+// re-enable scrolling of body
+document.body.style.overflow = 'visible';
